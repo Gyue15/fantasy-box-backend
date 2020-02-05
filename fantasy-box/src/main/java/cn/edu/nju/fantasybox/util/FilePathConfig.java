f255844708f4ed0ce32b475cd31fc783ff3a5937
gyue15 <786330297@qq.com>
2020-02-05 20:38:49 +0800

    分离开发环境配置文件；配置文件服务器

---
+++ fantasy-box/src/main/java/cn/edu/nju/fantasybox/util/FilePathConfig.java
@@ -0,0 +1,27 @@
+package cn.edu.nju.fantasybox.util;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class FilePathConfig implements WebMvcConfigurer {
+
+    @Getter
+    @Value("${file-service.local}")
+    private String localPath;
+
+    @Getter
+    @Value("${file-service.url-prefix}")
+    private String urlPrefix;
+
+    @Value("${file-service.url-pattern}")
+    private String urlPattern;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(urlPattern).addResourceLocations(localPath);
+    }
+}
