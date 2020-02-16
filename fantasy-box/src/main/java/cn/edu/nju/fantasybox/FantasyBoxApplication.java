0df0bc55f10c84f32e13c714379d7c565764e471
gyue15 <786330297@qq.com>
2020-02-16 23:39:36 +0800

    服务器配置

--- fantasy-box/src/main/java/cn/edu/nju/fantasybox/FantasyBoxApplication.java
+++ fantasy-box/src/main/java/cn/edu/nju/fantasybox/FantasyBoxApplication.java
@@ -1,9 +1,7 @@
 package cn.edu.nju.fantasybox;
 
-import cn.edu.nju.fantasybox.util.RSAEncrypt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("cn.edu.nju.fantasybox.mapper")
