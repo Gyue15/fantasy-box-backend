9c2053f57e3137ef0157fa0fb2139fec22a24c9f
gyue15 <786330297@qq.com>
2020-04-06 19:48:31 +0800

    尝试修复服务器文件bug

--- fantasy-box/src/main/java/cn/edu/nju/fantasybox/configuration/WebMvcConfig.java
+++ fantasy-box/src/main/java/cn/edu/nju/fantasybox/configuration/WebMvcConfig.java
@@ -18,7 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authenticationInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(authenticationInterceptor()).addPathPatterns("/api/**").excludePathPatterns(urlPattern);
     }
 
     @Bean
