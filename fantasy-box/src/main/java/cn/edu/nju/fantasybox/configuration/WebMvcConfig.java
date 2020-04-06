f480bb2359cb232209c3ce90e6299b8665a579e7
gyue15 <786330297@qq.com>
2020-04-06 19:42:43 +0800

    尝试修复文件访问的bug

--- fantasy-box/src/main/java/cn/edu/nju/fantasybox/configuration/WebMvcConfig.java
+++ fantasy-box/src/main/java/cn/edu/nju/fantasybox/configuration/WebMvcConfig.java
@@ -1,18 +1,30 @@
 package cn.edu.nju.fantasybox.configuration;
 
+import cn.edu.nju.fantasybox.configuration.interceptor.AuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class FilePathConfig implements WebMvcConfigurer {
-
+public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${file-service.local}")
     private String localPath;
 
     @Value("${file-service.url-pattern}")
     private String urlPattern;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    public AuthenticationInterceptor authenticationInterceptor() {
+        return new AuthenticationInterceptor();
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
